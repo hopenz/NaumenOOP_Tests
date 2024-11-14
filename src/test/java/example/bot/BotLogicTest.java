0ff0d3522926e2c896864c24ec3b2e7eaaa2e4b6
hopenz <petrushina_03@inbox.ru>
2024-11-14 15:15:01 +0500

    Исправила замечания

--- src/test/java/example/bot/BotLogicTest.java
+++ src/test/java/example/bot/BotLogicTest.java
@@ -38,18 +38,85 @@ public class BotLogicTest {
     /**
      * Тестирование команды /test
      * Проверка корректности проверки ответов
+     * Оба ответа ПРАВИЛЬНЫЕ
      */
     @Test
-    void commandTestTest() {
+    void commandTestTwoAnswersAreCorrectTest() {
         botLogic.processCommand(user, "/test");
-        //Сообщение бота: Вычисление 10^2
+
+        Assertions.assertEquals("Вычислите степень: 10^2", fakeTestBot.getMessageByIndex(0));
         botLogic.processCommand(user, "100");
-        Assertions.assertEquals("Правильный ответ!",
-                fakeTestBot.getMessageByIndex(1));
-        //Сообщение бота: Вычислить 2 + 2 * 2
-        botLogic.processCommand(user, "1234");
-        Assertions.assertEquals("Вы ошиблись, верный ответ: 6",
-                fakeTestBot.getMessageByIndex(3));
+        Assertions.assertEquals("Правильный ответ!", fakeTestBot.getMessageByIndex(1));
+
+        Assertions.assertEquals("Сколько будет 2 + 2 * 2", fakeTestBot.getMessageByIndex(2));
+        botLogic.processCommand(user, "6");
+        Assertions.assertEquals("Правильный ответ!", fakeTestBot.getMessageByIndex(3));
+
+        Assertions.assertEquals("Тест завершен", fakeTestBot.getMessageByIndex(4));
+
+    }
+
+    /**
+     * Тестирование команды /test
+     * Проверка корректности проверки ответов
+     * Оба ответа НЕПРАВИЛЬНЫЕ
+     */
+    @Test
+    void commandTestTwoAnswersAreInCorrectTest() {
+        botLogic.processCommand(user, "/test");
+
+        Assertions.assertEquals("Вычислите степень: 10^2", fakeTestBot.getMessageByIndex(0));
+        botLogic.processCommand(user, "1");
+        Assertions.assertEquals("Вы ошиблись, верный ответ: 100", fakeTestBot.getMessageByIndex(1));
+
+        Assertions.assertEquals("Сколько будет 2 + 2 * 2", fakeTestBot.getMessageByIndex(2));
+        botLogic.processCommand(user, "1");
+        Assertions.assertEquals("Вы ошиблись, верный ответ: 6", fakeTestBot.getMessageByIndex(3));
+
+        Assertions.assertEquals("Тест завершен", fakeTestBot.getMessageByIndex(4));
+
+    }
+
+    /**
+     * Тестирование команды /test
+     * Проверка корректности проверки ответов
+     * ПЕРВЫЙ НЕПРАВИЛЬНЫЙ, ВТОРОЙ ПРАВИЛЬНЫЙ
+     */
+    @Test
+    void commandTestFirstAnswerIsInCorrectAndSecondAnswerIsCorrectTest() {
+        botLogic.processCommand(user, "/test");
+
+        Assertions.assertEquals("Вычислите степень: 10^2", fakeTestBot.getMessageByIndex(0));
+        botLogic.processCommand(user, "1");
+        Assertions.assertEquals("Вы ошиблись, верный ответ: 100", fakeTestBot.getMessageByIndex(1));
+
+        Assertions.assertEquals("Сколько будет 2 + 2 * 2", fakeTestBot.getMessageByIndex(2));
+        botLogic.processCommand(user, "6");
+        Assertions.assertEquals("Правильный ответ!", fakeTestBot.getMessageByIndex(3));
+
+        Assertions.assertEquals("Тест завершен", fakeTestBot.getMessageByIndex(4));
+
+    }
+
+    /**
+     * Тестирование команды /test
+     * Проверка корректности проверки ответов
+     * ПЕРВЫЙ ПРАВИЛЬНЫЙ, ВТОРОЙ НЕ ПРАВИЛЬНЫЙ
+     */
+    @Test
+    void commandTestFirstAnswerIsCorrectAndSecondAnswerIsIncorrectTest() {
+        botLogic.processCommand(user, "/test");
+
+        Assertions.assertEquals("Вычислите степень: 10^2", fakeTestBot.getMessageByIndex(0));
+        botLogic.processCommand(user, "100");
+        Assertions.assertEquals("Правильный ответ!", fakeTestBot.getMessageByIndex(1));
+
+        Assertions.assertEquals("Сколько будет 2 + 2 * 2", fakeTestBot.getMessageByIndex(2));
+        botLogic.processCommand(user, "1");
+        Assertions.assertEquals("Вы ошиблись, верный ответ: 6", fakeTestBot.getMessageByIndex(3));
+
+        Assertions.assertEquals("Тест завершен", fakeTestBot.getMessageByIndex(4));
+
     }
 
     /**
@@ -59,13 +126,17 @@ public class BotLogicTest {
     @Test
     void commandNotifyTest() throws InterruptedException {
         botLogic.processCommand(user, "/notify");
-        // Текст напоминания
+        Assertions.assertEquals("Введите текст напоминания", fakeTestBot.getMessageByIndex(0));
         botLogic.processCommand(user, "I need to buy a car tomorrow :D");
-        // Время, через которое напомнить
+
+        Assertions.assertEquals("Через сколько секунд напомнить?", fakeTestBot.getMessageByIndex(1));
         botLogic.processCommand(user, "1");
-        Thread.sleep(666);
+
+        Assertions.assertEquals("Напоминание установлено", fakeTestBot.getMessageByIndex(2));
+        Thread.sleep(510);
         Assertions.assertEquals(3, fakeTestBot.getSizeListOfBotsAnswers());
-        Thread.sleep(666);
+        Thread.sleep(500);
+
         Assertions.assertEquals("Сработало напоминание: 'I need to buy a car tomorrow :D'",
                 fakeTestBot.getMessageByIndex(3));
     }
@@ -78,7 +149,7 @@ public class BotLogicTest {
     @Test
     void commandNotifyWithANegativeValueTest() {
         botLogic.processCommand(user, "/notify");
-        // Текст напоминания
+        Assertions.assertEquals("Введите текст напоминания", fakeTestBot.getMessageByIndex(0));
         botLogic.processCommand(user, "I need to buy a car tomorrow :D");
 
         Exception e = Assertions.assertThrows(IllegalArgumentException.class,
@@ -98,11 +169,15 @@ public class BotLogicTest {
 
         // вопрос на повторение добавляется при неправильном ответе
         botLogic.processCommand(user, "/test");
-        // Вычислить 10^2
+        Assertions.assertEquals("Вычислите степень: 10^2", fakeTestBot.getMessageByIndex(1));
         botLogic.processCommand(user, "100");
-        // Правильный ответ
+        Assertions.assertEquals("Правильный ответ!", fakeTestBot.getMessageByIndex(2));
+
+        Assertions.assertEquals("Сколько будет 2 + 2 * 2", fakeTestBot.getMessageByIndex(3));
         botLogic.processCommand(user, "7");
-        // Вы ошиблись
+        Assertions.assertEquals("Вы ошиблись, верный ответ: 6", fakeTestBot.getMessageByIndex(4));
+        Assertions.assertEquals("Тест завершен", fakeTestBot.getMessageByIndex(5));
+
         botLogic.processCommand(user, "/repeat");
         Assertions.assertEquals("Сколько будет 2 + 2 * 2",
                 fakeTestBot.getMessageByIndex(6));
